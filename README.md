db8eebc17fc48ceb096397a394313d535c7a0455
minoru.2021@icloud.com <minoru.2021@icloud.com>
2021-12-17 17:27:04 +0900

    トップ、検索フォーム

--- README.md
+++ README.md
@@ -15,3 +15,6 @@ For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
 # FoodAppCource
+
+## 参考Youtube
+https://www.youtube.com/watch?v=T5jwA3fvcHU&list=PLmnT6naTGy2RcFB4atBGXuS5RjlxDwIqQ
---
+++ lib/screens/home.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:food_app_cource/commons.dart';
+import 'package:food_app_cource/widgets/custom_text.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: white,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomText(
+                    text: "What would yout like to eat?",
+                    size: 18,
+                  ),
+                ),
+                Stack(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.notifications_none),
+                      onPressed: () {},
+                    ),
+                    Positioned(
+                      top: 12,
+                      right: 12,
+                      child: Container(
+                        height: 10,
+                        width: 10,
+                        decoration: BoxDecoration(
+                          color: red,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: grey,
+                      offset: Offset(1, 1),
+                      blurRadius: 4,
+                    )
+                  ],
+                ),
+                child: ListTile(
+                  leading: Icon(Icons.search, color: red),
+                  title: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Find food and restaurent",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                  trailing: Icon(Icons.filter_list, color: red),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
